952f3910af502d8e441885ff8ae12a84cdc3c5e2
John Giraldo <94002015+johnggiraldo@users.noreply.github.com>
2021-11-09 20:01:00 -0500

    Finalizados los ejercicios
    Muchas gracias.

---
+++ ejercicio_3.py
@@ -0,0 +1,28 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese primero su nombre y luego su apellido
+# Capture ambos datos e imprima su nombre completo
+print('Ingrese por consola su nombre/s:')
+nombre = str(input())
+
+print('Ingrese por consola su apellido/s:')
+apellido = str(input())
+
+# Imprima su nombre completo
+print('Nombre: ', nombre, ' ',  apellido)
+# Almacenar su nombre completo en una variable
+# nombre_completo = .....
+fullname = nombre+apellido
+# Imprimir la cantidad de letras que posee su nombre completo
+# cantidad_letras = len(....)
+cantidad_letras=len(fullname)
+print('Mi nombre tiene', cantidad_letras, 'letras')
---
+++ profundizacion_1.py
@@ -0,0 +1,42 @@
+# Tipos de variables [Python]
+# Ejercicios de profundización
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# NOTA: 
+# Estos ejercicios son de mayor dificultad que los de clase y práctica.
+# Están pensados para aquellos con conocimientos previo o que dispongan
+# de mucho más tiempo para abordar estos temas por su cuenta.
+# Requiere mayor tiempo de dedicación e investigación autodidacta.
+
+# IMPORTANTE: NO borrar los comentarios en VERDE o NARANJA
+
+# Ejercicios de práctica con números
+'''
+Enunciado:
+Realice un calculadora, se ingresará por línea de comando dos
+números reales y se deberá calcular todas las operaciones entre ellos:
+A) Suma
+B) Resta
+C) Multiplicación
+D) División
+E) Exponente/Potencia
+
+- Para todos los casos se debe imprimir en pantalla el resultado aclarando
+  la operación realizada en cada caso y con que números
+  se ha realizado la operación
+  ej: La suma entre 4.2 y 6.5 es 10.7
+'''
+
+print('¡Nuestra primera calculadora!')
+# Empezar aquí la resolución del ejercicio
+print('Ingrese el primer número: ')
+num_1 = int(input())
+print('Ingrese el segundo número: ')
+num_2 = int(input())
+print('Suma:', num_1 + num_2)
+print('Resta:', num_1 - num_2)
+print('Multiplicación:', num_1 * num_2)
+print('División:', num_1/num_2)
+print('Exponente/Potencia:', num_1 ** num_2)
